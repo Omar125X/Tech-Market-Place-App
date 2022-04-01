5771309e925805257e1cafe0db2a59a24827107b
OmarElsh <amrmawaheb65@gmail.com>
2022-04-02 02:16:43 +0300

    InitialSetup

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+
 const Stack = createStackNavigator();
 
 const theme = {
@@ -13,6 +16,17 @@ const theme = {
 }
 
 const App = () => {
+
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf")
+  });
+
+  if (!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
